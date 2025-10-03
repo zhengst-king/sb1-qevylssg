f8ee2db944982f1370385c542386f0ef8015c189
zhengst-king <veggiebus@gmail.com>
2025-10-03 10:57:46 -0700

    Updated TVSeriesWatchlistPage.tsx

--- src/components/TVSeriesWatchlistPage.tsx
+++ src/components/TVSeriesWatchlistPage.tsx
@@ -812,7 +812,7 @@ export function TVSeriesWatchlistPage() {
             </div>
           </div>
         ) : (
-          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-6">
+          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4">
             {sortedMovies.map((movie) => (
               <EnhancedTVSeriesCard
                 key={movie.id}
