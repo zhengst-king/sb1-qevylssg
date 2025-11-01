2a5cb9ff39e06e3b8115183d2f92ed9dac72e74b
zhengst-king <veggiebus@gmail.com>
2025-11-01 09:17:18 -0700

    Updated TagCategoryPage.tsx

--- src/components/TagCategoryPage.tsx
+++ src/components/TagCategoryPage.tsx
@@ -253,9 +253,6 @@ export function TagCategoryPage() {
               
               {/* Loading State */}
               {loading && (
-              
-              {/* Loading State */}
-              {loading && (
                 <div className="flex items-center justify-center py-12">
                   <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-600"></div>
                 </div>
